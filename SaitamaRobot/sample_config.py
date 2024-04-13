4afc9fab2f60269bf3523a0b4da343dd41a2f30d
Shadizinho <163308924+AnimeHW@users.noreply.github.com>
2024-04-13 11:07:34 -0300

    Update sample_config.py

--- SaitamaRobot/sample_config.py
+++ SaitamaRobot/sample_config.py
@@ -1,4 +1,3 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
@@ -9,54 +8,46 @@ def get_user_list(config, key):
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID. 
-    EVENT_LOGS = -1001190806654 #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 21732614  
+    API_HASH = "ff91cccbb310e68d0b93320abb2d229d"
+    TOKEN = "6563345274:AAH7LT6Mqc3_ZoMiJgA-ox_s8zf_73MCxQ0"  
+    OWNER_ID = 6710268098 
+    OWNER_USERNAME = "Shadizinho"
+    SUPPORT_CHAT = 'IGRISSx_sup'  
+    JOIN_LOGGER = -1002069292433 
+    EVENT_LOGS = -1001984580263 
     
-    #RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
+    
+    SQLALCHEMY_DATABASE_URI = 'mongodb+srv://shadi:12341234@igrisxbot.dqashbw.mongodb.net/'
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "ltwfo4Z7BG3tF4vQn~sWauW6q06ZpICxEtueM09l5yceJKkR4uxPV2O8PbwY1IEs" 
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
-    #OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    SUDO_USERS = get_user_list('elevated_users.json', 'sudos')
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    SUDO_USERS = get_user_list('6959003848', '7080925947')
     DEV_USERS = get_user_list('elevated_users.json', 'devs')
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     SUPPORT_USERS = get_user_list('elevated_users.json', 'supports')
-    #List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGER_USERS = get_user_list('elevated_users.json', 'tigers')
     WHITELIST_USERS = get_user_list('elevated_users.json', 'whitelists')
-    DONATION_LINK = None  # EG, paypal
+    DONATION_LINK = None  
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    DEL_CMDS = True 
     STRICT_GBAN = True
-    WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
-    BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
-    TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
-    WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
-    AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
-    BL_CHATS = []  # List of groups that you want blacklisted.
+    WORKERS = 8  
+    BAN_STICKER = 'CAACAgQAAxkBAAEXXIpmGojxnkWCeFAtfDs2w2ggbkOf6wACdRMAAue1kFOMHaxTFlb8_TQE' 
+    ALLOW_EXCL = True  
+    CASH_API_KEY = 'XDG2I0AAURMP8QXI' 
+    TIME_API_KEY = 'KNJIWN8SZBVX'
+    WALL_API = 'https://api.alphacoders.com/3.0?type=desktop&method=wallpaper_info&id=865098'  
+    AI_API_KEY = 'AIzaSyAyNlhxI4q-fcFEH2eYUjU9xc9fYZ5Q66s' 
+    BL_CHATS = []  
     SPAMMERS = None
 
 class Production(Config):
